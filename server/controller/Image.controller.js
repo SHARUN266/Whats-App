da55a319e1a5a4a94ad1ee0d88ddd23c3944ae37
SHARUN266 <sharunattari266@gmail.com>
2023-01-29 18:44:05 +0530

    HG

--- server/controller/Image.controller.js
+++ server/controller/Image.controller.js
@@ -1,36 +1,34 @@
 import grid from "gridfs-stream";
 import mongoose from "mongoose";
-const xss = require('xss-clean');
-const url="http://localhost:8000";
-let gfs,gridfsBucket;
-const conn=mongoose.connection;
-conn.once('open',()=>{
-    gridfsBucket= new mongoose.mongo.GridFSBucket(conn.db,{
-        bucketName:"fs"
-    });
+import xss from "xss-clean";
+const url = "http://localhost:8000";
+let gfs, gridfsBucket;
+const conn = mongoose.connection;
+conn.once("open", () => {
+  gridfsBucket = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: "fs",
+  });
 
-    gfs=grid(conn.db,mongoose.mongo);
-    gfs.collection('fs')
+  gfs = grid(conn.db, mongoose.mongo);
+  gfs.collection("fs");
+});
+export const uploadFile = (req, res) => {
+  if (!req.file) {
+    return res.status(404).json("File not found");
+  }
 
-})
-export const uploadFile=(req,res)=>{
-   if(!req.file){
-     return res.status(404).json("File not found")
-   }
-   
-   const imageUrl=`${url}/file/${req.file.filename}`;
+  const imageUrl = `${url}/file/${req.file.filename}`;
 
-   return res.status(200).json(imageUrl)
-}
+  return res.status(200).json(imageUrl);
+};
 
-export const getImage=async(req,res)=>{
-    try{
-        const file =await gfs.files.findOne({filename:req.params.filename});
-        const readStream=gridfsBucket.openDownloadStream(file._id);
-        readStream.pipe(res)
-
-    }catch(e){
-        const sanitizedErrorMessage = xss(e.message);
-        res.status(404).send(sanitizedErrorMessage);
-    }
-}
+export const getImage = async (req, res) => {
+  try {
+    const file = await gfs.files.findOne({ filename: req.params.filename });
+    const readStream = gridfsBucket.openDownloadStream(file._id);
+    readStream.pipe(res);
+  } catch (e) {
+    const sanitizedErrorMessage = xss(e.message);
+    res.status(404).send(sanitizedErrorMessage);
+  }
+};
