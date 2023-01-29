95ae36dba2188945053454b4b1b96779f7d486df
SHARUN266 <sharunattari266@gmail.com>
2023-01-29 18:52:42 +0530

    HG

--- Socket/index.js
+++ Socket/index.js
@@ -1,48 +1,64 @@
-import express from "express"
+import express from "express";
+import http from "http";
+import socketio from "socket.io";
+import jwt from "jsonwebtoken";
+
 const app = express();
-import http from "http"
 const server = http.createServer(app);
-import {Server} from "socket.io"
-const io = new Server(5000, {
-    cors: {
-        origin: 'http://localhost:3000',
-    }, 
-})
-let users=[]
+const io = socketio(server);
+
+const SECRET_KEY = "mysecretkey";
+
+let users = [];
+
 const addUser = (userData, socketId) => {
-    !users.some(user => user.uid === userData.uid) && users.push({ ...userData, socketId });
-}
+    !users.some((user) => user.uid === userData.uid) &&
+        users.push({ ...userData, socketId });
+};
+
 const removeUser = (socketId) => {
-    users = users.filter(user => user.socketId !== socketId);
-}
-const getUser=(userId)=>{
- 
-    return users.find(user=>user.uid===userId)
-  
-}
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  
-    //connect
-    socket.on("addUser", userData => {
-      
-        addUser(userData, socket.id);
-       
-        io.emit("getusers", users);
-        
-    })
-    // Send message
-    socket.on('sendMessage', (data) => {
-        const user = getUser(data.receiverId);
-        console.log(user,"sharun")
-   
-        io.to(user?.socketId).emit('getMessage', data)
-    })
-     //disconnect
-     socket.on('disconnected', () => {
-        console.log('user disconnected');
-        removeUser(socket.id);
-        io.emit('getUsers', users);
-    })
+    users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+    return users.find((user) => user.uid === userId);
+};
+
+io.use((socket, next) => {
+    const token = socket.handshake.query.token;
+    if (!token) return next(new Error("authentication error"));
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+        if (err) return next(new Error("authentication error"));
+        socket.decoded = decoded;
+        next();
+    });
 });
 
+io.on("connection", (socket) => {
+    console.log("a user connected");
+
+    //connect
+    socket.on("addUser", (userData) => {
+        addUser(userData, socket.id);
+        io.emit("getusers", users);
+    });
+
+    // Send message
+    socket.on("sendMessage", (data) => {
+        const user = getUser(data.receiverId);
+       
+
+        io.to(user?.socketId).emit("getMessage", data);
+    });
+
+    //disconnect
+    socket.on("disconnected", () => {
+        console.log("user disconnected");
+        removeUser(socket.id);
+        io.emit("getUsers", users);
+    });
+});
+
+server.listen(5000, () => {
+    console.log("server started on port 5000");
+});
